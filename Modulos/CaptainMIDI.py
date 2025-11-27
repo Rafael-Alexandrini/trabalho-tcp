ab81bc32c24979b630dcecd35eb2f51f2021ffc5
Alexandre <70719136+AlexandreIM@users.noreply.github.com>
2025-11-27 00:40:55 -0300

    Variaveis gerais

--- Modulos/CaptainMIDI.py
+++ Modulos/CaptainMIDI.py
@@ -3,7 +3,7 @@ from TocadorMidi import TocadorMidi
 from DecodificadorMidi import DecodificadorMidi
 
 def play():
-    global janela_principal, tocador, decodificador, caixa_oitava, botao_play
+    global janela_principal, tocador, decodificador, caixa_oitava
     tocador.parar()
     decodificador.set_bpm_padrao(janela_principal.get_bpm())
     decodificador.set_instrumento_padrao(get_instrument_number(janela_principal.get_instrument()))
@@ -11,16 +11,7 @@ def play():
     decodificador.set_volume_padrao(janela_principal.get_volume())
     texto = janela_principal.get_text()
     sequencia = decodificador.texto_para_sequencia_midi(texto)
-    tocador.tocar(sequencia, stop)
-    janela_principal.set_button_text(botao_play, "Parar")
-    janela_principal.set_button_commmand(botao_play, stop)    
-
-def stop():
-    global tocador, janela_principal, botao_play
-    tocador.parar()
-    janela_principal.set_button_text(botao_play, "Tocar")
-    janela_principal.set_button_commmand(botao_play, play)  
-
+    tocador.tocar(sequencia)
 
 
 INSTRUMENTO_INICIAL = 0
@@ -29,8 +20,6 @@ OITAVA_INICIAL = 4
 VOLUME_INICIAL = 60
 tocador = TocadorMidi()
 decodificador = DecodificadorMidi(BPM_INICIAL, INSTRUMENTO_INICIAL, OITAVA_INICIAL, VOLUME_INICIAL)
-
-
 
 TITLE = "Captain MIDI"
 MAIN_WINDOW_PADDING = (3, 3, 12, 12)
@@ -59,33 +48,32 @@ janela_principal.add_menu_command('Load txt', lambda: print('Load txt file'))
 janela_principal.create_text_label("Captain MIDI", 0, 0, 'n', "Arial 20 bold", 4)
 
 janela_principal.create_text_label("Oitava:", 0, 1, 'sw')
-caixa_oitava = janela_principal.create_combobox(OITAVAS, janela_principal.get_octave_var(), 0, 2, 'nwe')
+oitava = janela_principal.init_int_var()
+caixa_oitava = janela_principal.create_combobox(OITAVAS, oitava, 0, 2, 'nwe')
 
 janela_principal.create_text_label("Instrumento:", 1, 1, 'sw')
-caixa_instrumento = janela_principal.create_combobox(INSTRUMENTOS, janela_principal.get_instrument_var(), 1, 2, 'nwe')
+instrumento = janela_principal.init_string_var()
+caixa_instrumento = janela_principal.create_combobox(INSTRUMENTOS, instrumento, 1, 2, 'nwe')
 
-escala_volume = janela_principal.create_horizontal_scale_with_label("Volume :", 0, 127, VOLUME_INICIAL, janela_principal.get_vol_var(), 
-                                                    SCALE_LENGTH, 2, 2, 'nwe')
+volume = janela_principal.init_int_var()
+escala_volume = janela_principal.create_horizontal_scale_with_label("Volume :", 0, 100, 20, volume, 
+                                                                    SCALE_LENGTH, 2, 2, 'nwe')
 
-escala_bpm = janela_principal.create_horizontal_scale_with_label("BPM :", 40, 240, BPM_INICIAL, janela_principal.get_bpm_var(), 
-                                                    SCALE_LENGTH, 3, 2, 'nwe')
+bpm = janela_principal.init_int_var()
+escala_bpm = janela_principal.create_horizontal_scale_with_label("BPM :", 0, 300, 120, bpm, 
+                                                                 SCALE_LENGTH, 3, 2, 'nwe')
 
 texto = janela_principal.create_text_widget(50, 10, TEXTO_INICIAL, 0, 3, "nwes", 4)
 
-botao_play = janela_principal.create_button("Tocar", play, 3, 4, "we")
+botao_play = janela_principal.create_button("Play", play, 3, 4, "we")
 
 janela_principal.create_text_label(TABELA_CARACTERES_ESQ, 0, 5, 'ne')
 janela_principal.create_text_label(TABELA_FUNCAO_ESQ, 1, 5, 'nw')
 janela_principal.create_text_label(TABELA_CARACTERES_DIR, 2, 5, 'ne')
 janela_principal.create_text_label(TABELA_FUNCAO_DIR, 3, 5, 'nw')
 
-#janela_principal.bind_combobox_event(caixa_instrumento, 
-#    lambda e: print(f"Selected instrument: {get_instrument_number(janela_principal.get_instrument())}"))
-
 janela_principal.set_paddings(5, 5)
 janela_principal.set_weights(4, 6)
 
 janela_principal.start_mainloop()
 tocador.parar()
-
-
--- Modulos/Janela.py
+++ Modulos/Janela.py
@@ -5,8 +5,6 @@ class Janela:
     def __init__(self, title : str, frame_padding : tuple[int, int, int, int], menu_label : str = 'File'):
         self.__root = Tk()
         self.__root.title(title)
-        style = ttk.Style()
-        style.theme_use('clam')  
 
         self.__mainframe = ttk.Frame(self.__root, padding=frame_padding)
         self.__mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -18,12 +16,6 @@ class Janela:
         self.__root['menu'] = self.__menubar
         self.__menu = Menu(self.__menubar, tearoff=0)
         self.__menubar.add_cascade(menu=self.__menu, label=menu_label)
-
-        self.__text = Text()
-        self.__volume = IntVar()
-        self.__bpm = IntVar()
-        self.__instrument = StringVar()
-        self.__octave = IntVar()
 
     def start_mainloop(self):
         self.__root.mainloop()
@@ -41,11 +33,17 @@ class Janela:
     def add_menu_command(self, label: str, command):
         self.__menu.add_command(label=label, command=command)
 
+    def init_int_var(self):
+        return IntVar()
+    
+    def init_string_var(self):
+        return StringVar()
+
     def create_text_widget(self, width: int, height: int, initial_text: str, column: int, row: int, sticky: str, colspan: int = 1):
-        self.__text = Text(self.__mainframe, width=width, height=height)
-        self.__text.grid(column=column, row=row, sticky=sticky, columnspan=colspan)
-        self.__text.insert("1.0", initial_text)
-        return self.__text
+        text = Text(self.__mainframe, width=width, height=height)
+        text.grid(column=column, row=row, sticky=sticky, columnspan=colspan)
+        text.insert("1.0", initial_text)
+        return text
     
     def create_button(self, text: str, command, column: int, row: int, sticky: str):
         button = ttk.Button(self.__mainframe, text=text, command=command)
@@ -63,12 +61,12 @@ class Janela:
         label.grid(column=column, row=row, sticky=sticky, columnspan=colspan)
         return label
     
-    def create_combobox(self, values: tuple, textvariable: StringVar, column: int, row: int, sticky: str):
+    def create_combobox(self, values: tuple, textvariable: StringVar, column: int, row: int, sticky: str, initial_index: int = 0):
         combobox = ttk.Combobox(self.__mainframe, textvariable=textvariable)
         combobox.grid(column=column, row=row, sticky=sticky)
         combobox['values'] = values
         combobox.state(["readonly"])
-        combobox.current(0)
+        combobox.current(initial_index)
         return combobox
     
     def __update_scale_label(self, label: ttk.Label, var: IntVar):
@@ -86,14 +84,14 @@ class Janela:
     def bind_combobox_event(self, box: ttk.Combobox, command):
         box.bind('<<ComboboxSelected>>', command)
 
-    def disable_text(self):
-        self.__text['state'] = 'disabled'
+    def disable_text(self, text_widget: Text):
+        text_widget['state'] = 'disabled'
 
-    def enable_text(self):
-        self.__text['state'] = 'normal'
+    def enable_text(self, text_widget: Text):
+        text_widget['state'] = 'normal'
 
-    def is_text_enabled(self) -> bool:
-        return self.__text['state'] == 'normal'
+    def is_text_enabled(self, text_widget: Text) -> bool:
+        return text_widget['state'] == 'normal'
 
     def disable_widget(self, widget: Widget):
         widget.state(['disabled'])
@@ -104,42 +102,24 @@ class Janela:
     def is_widget_enabled(self, widget: Widget) -> bool:
         return widget.instate(['!disabled'])
 
-    def get_text(self) -> str:
-        return self.__text.get("1.0", END).strip()
+    def get_text(self, text_widget: Text) -> str:
+        return text_widget.get("1.0", END).strip()
     
-    def get_volume(self) -> int:
-        return self.__volume.get()
-    
-    def get_vol_var(self) -> IntVar:
-        return self.__volume
-    
-    def get_bpm(self) -> int:
-        return self.__bpm.get()
-    
-    def get_bpm_var(self) -> IntVar:
-        return self.__bpm
-    
-    def get_octave_var(self) -> IntVar:
-        return self.__octave
-    
-    def get_instrument(self) -> str:
-        return self.__instrument.get()
-    
-    def get_instrument_var(self) -> StringVar:
-        return self.__instrument
+    def get_intvar_value(self, var: IntVar) -> int:
+        return var.get()
+ 
+    def get_strvar_string(self, var: StringVar) -> str:
+        return var.get()
 
     def get_combobox_current_index(self, combobox: ttk.Combobox) -> int:
         return combobox.current()
       
-    def set_text(self, text: str):
-        self.__text.delete("1.0", END)
-        self.__text.insert("1.0", text)
+    def set_text(self, text_widget: Text, text: str):
+        text_widget.delete("1.0", END)
+        text_widget.insert("1.0", text)
 
-    def set_volume(self, volume: int):
-        self.__volume.set(volume)
-    
-    def set_bpm(self, bpm: int):
-        self.__bpm.set(bpm)
+    def set_intvar_value(self, var: IntVar, value: int):
+        var.set(value)
 
     def set_combobox_current_index(self, combobox: ttk.Combobox, index: int):
         combobox.current(index)
